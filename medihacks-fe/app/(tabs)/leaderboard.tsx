477b2b3c37c81fea5823977caaec62b4c0da9fa8
Musab Guma'a <musabgumaa@gmail.com>
2024-07-13 20:49:18 +0100

    make leaderboard functional

--- medihacks-fe/app/(tabs)/leaderboard.tsx
+++ medihacks-fe/app/(tabs)/leaderboard.tsx
@@ -1,226 +1,131 @@
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { StyleSheet, Image, Platform, View, Text } from 'react-native';
+import { StyleSheet, Image, Platform, View, Text, ActivityIndicator } from 'react-native';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
 import { Avatar, Divider } from '@rneui/themed';
+import { Component } from 'react';
+import { Badge } from '@rneui/base';
 
-export default function TabTwoScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: 'rgba(220, 20, 60, .3)' }}
-      headerImage={<Ionicons size={310} name="podium" style={styles.headerImage} />}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Weekly Leaderboard</ThemedText>
-      </ThemedView>
+export default class TabTwoScreen extends Component<{}, { awards: any, leaderboard: any }> {
 
+  constructor(props: any) {
+    super(props);
 
-      <ThemedText>See how your posture stacks up against others, and win weekly prizes by placing at the top of the leaderboard below!</ThemedText>
-      <ThemedText style={{ fontSize: 14, color: "lightgrey", fontWeight: "bold", marginLeft: 15 }}>Your Past Awards</ThemedText>
-      <View style={styles.awardContainer}>
-          <Avatar
-            size={64}
-            rounded
-            icon={{ name: 'trophy', type: 'ionicon', color: 'gold' }}
-            containerStyle={{
-              backgroundColor: "rgba(255, 255, 255, .2)",
-              borderWidth: 3,
-            }}
-          />
-          <Avatar
-            size={64}
-            rounded
-            icon={{ name: 'trophy', type: 'ionicon', color: 'silver' }}
-            containerStyle={{
-              backgroundColor: "rgba(255, 255, 255, .2)",
-              borderWidth: 3,
-            }}
-          />
-      </View>
+    this.state = {
+      awards: null,
+      leaderboard: null
+    };
 
-      <View style={{ marginHorizontal: 15, flexDirection: "row", marginVertical: 10 }}>
-        <Text style={{ fontSize: 14, color: "lightgrey", fontWeight: "bold", flex: 1 }}>Thu 14 - Thu 21</Text>
-        
-        <Text style={{ alignSelf: "flex-end", fontSize: 14, color: "lightgrey" }}>3 DAYS LEFT</Text>
-      </View>
-      <View style={styles.leaderboardContainer}>
+    this.getLeaderboard();
+  }
 
-        <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', gap: 16, paddingVertical: 13, paddingRight: -6, paddingLeft: 26, backgroundColor: "rgba(255, 255, 255, .2)", borderTopLeftRadius: 13, borderTopRightRadius: 13 }}>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            <ThemedText style={{ fontWeight: "bold" }}>Score</ThemedText>
-          </View>
-          <View style={{ flex: 4, alignSelf: 'stretch' }}>
-            <ThemedText style={{ fontWeight: "bold" }}>Name</ThemedText>
-          </View>
-          <View style={{ flex: 1, alignSelf: 'stretch', marginRight: -4 }}>
-            <Ionicons name="trophy-outline" size={24} color="white" />
-          </View>
+  async getLeaderboard() {
+    await fetch("https://36vojn4clhxqjoevx4zoptgnky0zcfzd.lambda-url.eu-west-2.on.aws/", {
+      method: "POST",
+      body: JSON.stringify({
+        device_name: "test_device"
+      })
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        this.setState({ awards: data.body.awards, leaderboard: [data.body.leaderboard] });
+      });
+  }
+
+  render() {
+    return (
+      <ParallaxScrollView
+        headerBackgroundColor={{ light: '#D0D0D0', dark: 'rgba(220, 20, 60, .3)' }}
+        headerImage={<Ionicons size={310} name="podium" style={styles.headerImage} />}>
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText type="title">Weekly Leaderboard</ThemedText>
+        </ThemedView>
+
+        <ThemedText>See how your posture stacks up against others, and win weekly prizes by placing at the top of the leaderboard below!</ThemedText>
+        <ThemedText style={{ fontSize: 14, color: "lightgrey", fontWeight: "bold", marginLeft: 15 }}>Your Past Awards</ThemedText>
+
+        {
+          this.state.awards ? (
+            <View style={styles.awardContainer}>
+
+              {
+                ["gold", "silver", "bronze"].filter(x => this.state.awards[x] > 0).map((x: string, k: number) =>
+                  <View key={k}>
+                    <Avatar
+                      size={64}
+                      rounded
+                      icon={{ name: 'trophy', type: 'ionicon', color: x == "bronze" ? "rgb(205, 127, 50)" : x }}
+                      containerStyle={{
+                        backgroundColor: "rgba(255, 255, 255, .2)",
+                        borderWidth: 3,
+                      }}
+                    />
+                    <Badge
+                      status="primary"
+                      value={this.state.awards[x].toString()}
+                      containerStyle={{ position: 'absolute', top: 5, left: 45 }}
+                      badgeStyle={{ borderWidth: 0 }}
+                    />
+                  </View>
+                )
+              }
+            </View>
+          ) : <ActivityIndicator size="large" style={{ marginTop: 20 }} />
+        }
+
+        <View style={{ marginHorizontal: 15, flexDirection: "row", marginVertical: 10 }}>
+          <Text style={{ fontSize: 14, color: "lightgrey", fontWeight: "bold", flex: 1 }}>Thu 14 - Thu 21</Text>
+
+          <Text style={{ alignSelf: "flex-end", fontSize: 14, color: "lightgrey" }}>3 DAYS LEFT</Text>
         </View>
 
-        <Divider style={styles.dividerCol} width={3}/>
+        {
+          this.state.leaderboard ?
 
-        <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', gap: 16, left: 16, paddingVertical: 13, paddingHorizontal: 10 }}>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            <ThemedText>63278</ThemedText>
-          </View>
-          <View style={{ flex: 4, alignSelf: 'stretch' }}>
-            <ThemedText>Test Name</ThemedText>
-          </View>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            <Ionicons name="trophy" size={24} color="gold" />
-          </View>
-        </View>
+            <View style={styles.leaderboardContainer}>
 
-        <Divider style={styles.dividerCol} width={2}/>
+              <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', gap: 16, paddingVertical: 13, paddingRight: -6, paddingLeft: 26, backgroundColor: "rgba(255, 255, 255, .2)", borderTopLeftRadius: 13, borderTopRightRadius: 13 }}>
+                <View style={{ flex: 1, alignSelf: 'stretch' }}>
+                  <ThemedText style={{ fontWeight: "bold" }}>Score</ThemedText>
+                </View>
+                <View style={{ flex: 4, alignSelf: 'stretch' }}>
+                  <ThemedText style={{ fontWeight: "bold" }}>Name</ThemedText>
+                </View>
+                <View style={{ flex: 1, alignSelf: 'stretch', marginRight: -4 }}>
+                  <Ionicons name="trophy-outline" size={24} color="white" />
+                </View>
+              </View>
 
-        <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', gap: 16, left: 16, paddingVertical: 13, paddingHorizontal: 10 }}>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            <ThemedText>32612</ThemedText>
-          </View>
-          <View style={{ flex: 4, alignSelf: 'stretch' }}>
-            <ThemedText>Test Name</ThemedText>
-          </View>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            <Ionicons name="trophy" size={24} color="silver" />
-          </View>
-        </View>
+              {
+                this.state.leaderboard.map((x: any, k: number) =>
+                  <View key={k}>
+                    <Divider style={styles.dividerCol} width={3} />
 
-        <Divider style={styles.dividerCol} width={2}/>
+                    <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', gap: 16, left: 16, paddingVertical: 13, paddingHorizontal: 10 }}>
+                      <View style={{ flex: 1, alignSelf: 'stretch' }}>
+                        <ThemedText>{x.score}</ThemedText>
+                      </View>
+                      <View style={{ flex: 4, alignSelf: 'stretch' }}>
+                        <ThemedText style={{ fontFamily: "monospace" }}>{x.DeviceName}</ThemedText>
+                      </View>
+                      <View style={{ flex: 1, alignSelf: 'stretch' }}>
+                        {k < 3 && <Ionicons name="trophy" size={24} color={["gold", "silver", "bronze"][k]} />}
+                      </View>
+                    </View>
+                  </View>
+                )
+              }
 
-        <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', gap: 16, left: 16, paddingVertical: 13, paddingHorizontal: 10 }}>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            <ThemedText>29874</ThemedText>
-          </View>
-          <View style={{ flex: 4, alignSelf: 'stretch' }}>
-            <ThemedText>Test Name</ThemedText>
-          </View>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            <Ionicons name="trophy" size={24} color="rgb(205, 127, 50)" />
-          </View>
-        </View>
-
-        <Divider style={styles.dividerCol} width={2}/>
-
-        <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', gap: 16, left: 16, paddingVertical: 13, paddingHorizontal: 10 }}>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            <ThemedText>23212</ThemedText>
-          </View>
-          <View style={{ flex: 4, alignSelf: 'stretch' }}>
-            <ThemedText>Test Name</ThemedText>
-          </View>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-          </View>
-        </View>
-
-        <Divider style={styles.dividerCol} width={2}/>
-
-        <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', gap: 16, left: 16, paddingVertical: 13, paddingHorizontal: 10 }}>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            <ThemedText>23212</ThemedText>
-          </View>
-          <View style={{ flex: 4, alignSelf: 'stretch' }}>
-            <ThemedText>Test Name</ThemedText>
-          </View>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-          </View>
-        </View>
-
-        <Divider style={styles.dividerCol} width={2}/>
-
-        <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', gap: 16, left: 16, paddingVertical: 13, paddingHorizontal: 10 }}>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            <ThemedText>23212</ThemedText>
-          </View>
-          <View style={{ flex: 4, alignSelf: 'stretch' }}>
-            <ThemedText>Test Name</ThemedText>
-          </View>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-          </View>
-        </View>
-
-        <Divider style={styles.dividerCol} width={2}/>
-
-        <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', gap: 16, left: 16, paddingVertical: 13, paddingHorizontal: 10 }}>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            <ThemedText>23212</ThemedText>
-          </View>
-          <View style={{ flex: 4, alignSelf: 'stretch' }}>
-            <ThemedText>Test Name</ThemedText>
-          </View>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-          </View>
-        </View>
-
-        <Divider style={styles.dividerCol} width={2}/>
-
-        <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', gap: 16, left: 16, paddingVertical: 13, paddingHorizontal: 10 }}>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            <ThemedText>23212</ThemedText>
-          </View>
-          <View style={{ flex: 4, alignSelf: 'stretch' }}>
-            <ThemedText>Test Name</ThemedText>
-          </View>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-          </View>
-        </View>
-
-        <Divider style={styles.dividerCol} width={2}/>
-
-        <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', gap: 16, left: 16, paddingVertical: 13, paddingHorizontal: 10 }}>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            <ThemedText>23212</ThemedText>
-          </View>
-          <View style={{ flex: 4, alignSelf: 'stretch' }}>
-            <ThemedText>Test Name</ThemedText>
-          </View>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-          </View>
-        </View>
-
-        <Divider style={styles.dividerCol} width={2}/>
-
-        <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', gap: 16, left: 16, paddingVertical: 13, paddingHorizontal: 10 }}>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            <ThemedText>23212</ThemedText>
-          </View>
-          <View style={{ flex: 4, alignSelf: 'stretch' }}>
-            <ThemedText>Test Name</ThemedText>
-          </View>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-          </View>
-        </View>
-
-        <Divider style={styles.dividerCol} width={2}/>
-
-        <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', gap: 16, left: 16, paddingVertical: 13, paddingHorizontal: 10 }}>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            <ThemedText>23212</ThemedText>
-          </View>
-          <View style={{ flex: 4, alignSelf: 'stretch' }}>
-            <ThemedText>Test Name</ThemedText>
-          </View>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-          </View>
-        </View>
-
-        <Divider style={styles.dividerCol} width={2}/>
-
-        <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row', gap: 16, left: 16, paddingVertical: 13, paddingHorizontal: 10 }}>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-            <ThemedText>23212</ThemedText>
-          </View>
-          <View style={{ flex: 4, alignSelf: 'stretch' }}>
-            <ThemedText>Test Name</ThemedText>
-          </View>
-          <View style={{ flex: 1, alignSelf: 'stretch' }}>
-          </View>
-        </View>
-
-      </View>
-    </ParallaxScrollView>
-  );
+            </View>
+            : <ActivityIndicator size="large" style={{ marginTop: 20 }} />
+        }
+      </ParallaxScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -252,7 +157,7 @@ const styles = StyleSheet.create({
     elevation: 14,
     marginTop: -8,
     backgroundColor: "black"
-   },
+  },
   dividerCol: {
     borderColor: "rgba(255, 255, 255, .2)"
   }
